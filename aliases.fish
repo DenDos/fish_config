4116f0a581427e2e3e7ccc6205f9bcf58075d244
DenDos <cotoha92@gmail.com>
2022-01-04 15:11:03 +0400

    tweak aliases

--- aliases.fish
+++ aliases.fish
@@ -1,39 +1,32 @@
-alias projects="cd ~/Documents/projects"
 alias ngrok="$HOME/ngrok"
+
+# VimR
+# alias vimr="open -a VimR.app $argv"
+alias open_vim_conf="nvim /Users/cotoha/.config/nvim"
+
 
 #Node Start
 alias bn="babel-node"
 alias bnr="babel-node server/index.js"
 alias node_start="nodemon --exec babel-node server/index.js"
 
+# Flutter
+alias flutter_build_runner_watch="flutter packages pub run build_runner watch --delete-conflicting-outputs"
+
 #Mongo
 alias mongo_start="/Users/cotoha/Documents/mongodb/bin/mongod"
 
-#Govno
+# Folders
+alias projects="cd ~/Documents/projects"
 alias govno="cd /Users/cotoha/Documents/govno"
-
-#Learning
 alias learning="cd /Users/cotoha/Documents/learning"
-alias arduino="cd /Users/cotoha/Documents/learning/arduino"
 
 #Projects
 alias apidocs="apidoc -i server/api/controllers -o server/apidoc"
-alias rikku="cd /Users/cotoha/Documents/projects/rikuu"
-alias tripsjar="cd /Users/cotoha/Documents/projects/Tripsjar"
-alias bid="cd /Users/cotoha/Documents/projects/bidzone.ca"
-alias kurt="cd /Users/cotoha/Documents/projects/kurt"
-alias resi="cd /Users/cotoha/Documents/projects/resi"
-alias surveyer="cd /Users/cotoha/Documents/projects/surveyer"
-alias calendar="cd /Users/cotoha/Documents/projects/calendar"
-alias weekcap="cd /Users/cotoha/Documents/projects/weekcap"
-alias cryptosato_services="cd /Users/cotoha/Documents/projects/cryptosato-services"
-alias pragmatico-api="cd /Users/cotoha/Documents/projects/pragmatico-api"
+alias surveyer="cd /Users/cotoha/Documents/projects/friday"
+alias friday="cd /Users/cotoha/Documents/projects/friday"
 
-alias br="bin/rake"
-alias bs="bin/rails"
-
-alias subl="open -a 'Sublime Text'"
-
+# GIT
 alias gb="git branch"
 alias gs="git status"
 alias gsn="git config --global user.name 'comeonandroid'"
@@ -42,8 +35,9 @@ alias gse="git config --global user.email 'comeonandroid@gmail.com'"
 alias gme="git config --global user.email 'cotoha92@gmail.com'"
 alias git_get_email="git config --global user.email"
 
-alias kill_ruby="sh /Users/cotoha/.config/fish/functions/kill_ruby.sh"
 
 # RAILS
+alias kill_ruby="sh /Users/cotoha/.config/fish/functions/kill_ruby.sh"
 alias br="bin/rake"
+alias bs="bin/rails"
 
