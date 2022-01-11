bb6eebc06e7dc06dff23915737581fa2703be455
DenDos <cotoha92@gmail.com>
2022-01-11 17:53:37 +0400

    tweak colors config

--- config.fish
+++ config.fish
@@ -1,14 +1,30 @@
 . "$HOME/.config/fish/aliases.fish"
 . "$HOME/.config/fish/functions/fzf_functions.fish"
 . "$HOME/.config/fish/functions/main_functions.fish"
+. "$HOME/.config/fish/colors.fish"
 
+# FLUTTER
+set -x PATH "/Users/cotoha/Documents/libs/flutter/bin" $PATH
+
+#ASDF SETUP
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+# NODE PATH
 set -g fish_user_paths "/usr/local/opt/node@10/bin" $fish_user_paths
-
-set fish_color_search_match --background='333'
 
 # Path to Oh My Fish install.
 set -gx OMF_PATH $HOME/.local/share/omf
 
-rbenv init - | source
+# PYENV SETUP
+set -x PATH "$HOME/.pyenv/bin" $PATH
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
+echo 'eval "$(pyenv init --path)"' >> ~/.profile
+echo 'if [ -n "$PS1" -a -n "$BASH_VERSION" ]; then source ~/.bashrc; fi' >> ~/.profile
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
-# source /opt/homebrew/opt/asdf/libexec/asdf.fish
+status is-login; and pyenv init --path | source
+status is-interactive; and pyenv init - | source
+
+
+#rbenv init - | source
